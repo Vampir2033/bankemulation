5e33952c103f538d5ffff10909bf694f362e1033
RDorogin <dorogin.ruslan99@yandex.ru>
2021-09-01 18:15:03 +0300

    Initial commit

---
+++ src/main/java/ru/dorogin/bankemulation/BankemulationApplication.java
@@ -0,0 +1,13 @@
+package ru.dorogin.bankemulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankemulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankemulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dorogin/bankemulation/BankemulationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dorogin.bankemulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankemulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
